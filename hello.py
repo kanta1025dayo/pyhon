20b5ae899d039f8f603e1e11a3efbe9f4b6f24a1
k-nakasone <enaioshi407@gmail.com>
2024-04-23 12:15:45 +0900

    commit

--- hello.py
+++ hello.py
@@ -1,14 +1,5 @@
-class test:
-    def __init__(self):
-        print('おおお')
-    def bmi(self):
-        print('あいう')
-bc = test()
-
-
-
-
-
-
-
-
+class TestClass:
+    def __init__(self,student_name):
+        self.student = student_name
+sr = TestClass('sapu')
+sr.student        
