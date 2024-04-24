a691831214832009d54be042b7983ff2880ad422
k-nakasone <enaioshi407@gmail.com>
2024-04-24 11:54:20 +0900

    なにかわからんコード

--- hello.py
+++ hello.py
@@ -1,3 +1,28 @@
-from tqdm import tqdm
-for x in tqdm([10**9]):
-    pass #便利な関数で実行にかかる時間かかった時間を取得
+import random
+def calc_game():
+    operators = ["+","-","*","/"]
+    
+    for _ in range(5):
+        operator = random.choice(operators)
+        num1 = random.randint(1,10)
+        num2 = random.randint(1,10)
+        num2
+        print(num1, num2, operator)
+        
+        if  operator == "+":
+            answer = num1 + num2
+        elif operator == "-":
+            answer = num1 - num2
+        elif operator == "*":
+            answer = num1 * num2       
+        elif operator == "/":
+            answer = num1 / num2     
+        user_input = imput(f"{num}{operator}{num2}は？")
+        
+        if user_imput == answer:
+            print("正解")
+        else:
+            print("不正解")
+                
+    
+    calc_game()
