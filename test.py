ebe47075f5f84f6d6b26e60e432d3e2a992e9124
k-nakasone <enaioshi407@gmail.com>
2024-04-25 14:29:50 +0900

    classについて

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-def test_try(base=100, height=20):
-   return base * height * 0.5
-test_try(base=20, height=30)
-#関数について
-#test_try(base=100, height=20):この部分に書いてもいいし
-#(base=20, height=30)ここにも書いてもいいいい、その場合後からかいたほうが優先される
-#上にも下にも処理内容をかかなければエラーが発生する
+class Person:
+    def __init__(self,name, nationality, age):
+        self.name = name
+        self.nationality = nationality
+        self.age = age
+    def hello(self, name):
+        print(f'{name}こんにちは{self.name}')
+        imanishi.hello('こん')
     
