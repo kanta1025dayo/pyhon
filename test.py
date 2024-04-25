34c02fa2264c2b35a9237ec4e1134c2ce2cbf298
k-nakasone <enaioshi407@gmail.com>
2024-04-25 10:06:13 +0900

    掛け算ゲーム

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-from datetime import date
-t = date.today()
-print(t)
+tozai = ["三条", "四条", "五条"]
+nanboku = ["堀川", "鳥丸", "川原町"]
+for i in tozai:
+    for j in nanboku:
+        cross = i+j
+print(cross)
